0572aa02d162b8259c4362525354bf53272709d9
Parth Kumar <parth.kumar.coox@gmail.com>
2025-07-28 22:21:06 +0530

    UI changes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg'
+              src='./images/wwaterfall.webp'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='https://images.pexels.com/photos/2880755/pexels-photo-2880755.jpeg'
+              src='./images/bali.webp'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='https://images.pexels.com/photos/1655166/pexels-photo-1655166.jpeg'
+              src='./images/waters.webp'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='https://images.pexels.com/photos/13352733/pexels-photo-13352733.jpeg'
+              src='./images/football.webp'
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='https://images.pexels.com/photos/1703312/pexels-photo-1703312.jpeg'
+              src='./images/desert.webp'
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
